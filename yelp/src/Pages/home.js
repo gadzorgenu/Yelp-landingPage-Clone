733f5f4b4d974d06aaa7d6b7bf6eff9dccc7ffa2
Georgina Adzorgenu <gadzorgenu@gmail.com>
2020-12-30 12:13:50 +0100

    Changes

--- yelp/src/Pages/home.js
+++ yelp/src/Pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, Image} from '@chakra-ui/react'
+import {Box, Image, Text} from '@chakra-ui/react'
 import AlertInfo from '../Components/Alert'
 import Navbar from '../Components/Navbar'
 import rest2 from '../Assets/rest2.jpg'
@@ -10,11 +10,16 @@ const Home = () => {
     return (
         <Box >
             <AlertInfo/>
-            <Image pos='absolute' src={rest2} alt='restaurant' h={{md:'750px'}} w={{md: '100%'}}/>
             <Box pos='relative'>
-                <Navbar/>
-                <SearchBar/>
-                <Services/>
+                <Image  src={rest2} alt='restaurant' h={{md:'750px'}} w={{md: '100%'}}/>
+                <Box pos='absolute'>
+                    <Navbar/>
+                    <SearchBar/>
+                    <Services/>
+                </Box>
+            </Box>
+            <Box>
+                <Text color='red.400' fontWeight='bold' textAlign='center' mt={10}>Find the Best Business in Town</Text>
             </Box>
         </Box>
     )
