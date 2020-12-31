0adc788e33637230d7ab897878980ce2e4573ef2
Georgina Adzorgenu <gadzorgenu@gmail.com>
2020-12-31 13:31:01 +0100

    NO change

--- yelp/src/Pages/home.js
+++ yelp/src/Pages/home.js
@@ -10,16 +10,16 @@ const Home = () => {
     return (
         <Box >
             <AlertInfo/>
-            <Box pos='relative'>
-                <Image pos='absolute'  src={rest2} alt='restaurant' h={{md:'750px'}} w={{md: '100%'}}/>
-                <Box pos='relative'>
+            <Box bgColor='red.400' h={{md:'750px'}}>
+                {/* <Image pos='absolute'  src={rest2} alt='restaurant' h={{md:'750px'}} w={{md: '100%'}}/> */}
+                <Box>
                     <Navbar/>
                     <SearchBar/>
                     <Services/>
                 </Box>
             </Box>
-            <Box pos='relative'>
-                <Text color='red.400' fontWeight='bold' textAlign='center' mt={10}>Find the Best Business in Town</Text>
+            <Box >
+                <Text color='red.400' fontWeight='bold' textAlign='center' fontSize='30px' mt={10}>Find the Best Business in Town</Text>
             </Box>
         </Box>
     )
