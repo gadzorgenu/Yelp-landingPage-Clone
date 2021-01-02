89bf064d436e1925ab3ca4600e8456bef047c8db
Georgina Adzorgenu <gadzorgenu@gmail.com>
2021-01-03 00:01:01 +0100

    Modified Collections

--- yelp/src/Components/Collection.js
+++ yelp/src/Components/Collection.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Box,Text,Image, Flex, Icon} from '@chakra-ui/react'
 
-const Collection = ({location, title, numOfPlaces, name}) => {
+const Collection = ({location, title, numOfPlaces, name, image}) => {
     return (
        <Box >
-           <Text color='blue.400' py={2}>{location}</Text>
-           <Text>{title}</Text>
-            <Flex>
-                <Text color='gray.300'>{numOfPlaces} .By </Text>
-                <Text color='blue.400' ml={1}>{name}</Text>
-            </Flex>
+           <Flex>
+                <Image src={image} alt='Food' />
+                <Box>
+                        <Text color='blue.400' py={2}>{location}</Text>
+                        <Text>{title}</Text>
+                        <Flex>
+                            <Text color='gray.300'>{numOfPlaces} .By </Text>
+                            <Text color='blue.400' ml={1}>{name}</Text>
+                        </Flex>
+                </Box>
+           </Flex>
        </Box>
     )
 }
